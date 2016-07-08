18416c91aec19ffab5be7d0e407812e6835e8eb1
keebie81 <keebie81@me.com>
2016-07-08 09:13:11 -0500

    .

---
+++ Blink/Blink/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Maker.Serial;
+using Microsoft.Maker.RemoteWiring;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Blink
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        IStream connection;
+        RemoteDevice arduino;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            connection = new NetworkSerial(new Windows.Networking.HostName("192.168.1.170"), 3030);
+            arduino = new RemoteDevice(connection);
+            connection.ConnectionEstablished += OnConnectionEstablished;
+
+            connection.begin(115200, SerialConfig.SERIAL_8N1);
+        }
+
+        private void OnConnectionEstablished()
+        {
+            var action = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, new Windows.UI.Core.DispatchedHandler(() =>
+                {
+                OnButton.IsEnabled = true;
+                OffButton.IsEnabled = true;
+            }));
+        }
+
+        private void OnButton_Click(object sender, RoutedEventArgs e)
+        {
+            arduino.digitalWrite(6, PinState.HIGH);
+        }
+
+        private void OffButton_Click(object sender, RoutedEventArgs e)
+        {
+            arduino.digitalWrite(6, PinState.LOW);
+        }
+    }
+}
